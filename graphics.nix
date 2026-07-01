818e4e563158a7f89ad4bab3c26908016e55bed3
solarfire <xatra169@gmail.com>
2026-07-01 00:09:46 -0400

    replace nvidia.package with nvidia.branch

--- graphics.nix
+++ graphics.nix
@@ -1,10 +1,13 @@
 { config, pkgs, ... }:
 {
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.latest;
-  hardware.nvidia.open = true;
   services.xserver.videoDrivers = [ "nvidia" ];
   nixpkgs.config.cudaSupport = true;
 
+  hardware.nvidia = {
+    branch = "bleeding_edge";
+    open = true;
+  };
+ 
   hardware.graphics = {
     enable = true;
     extraPackages = with pkgs; [
