7e11ec16d9b2211225285fac6f0235ff0f36bb65
solarfire <xatra169@gmail.com>
2026-07-17 04:03:12 -0400

    plasma-manager: merge programs = { plasma = {} } into programs.plasma

--- home-manager/plasma/default.nix
+++ home-manager/plasma/default.nix
@@ -9,31 +9,29 @@
 
   home-manager.users.solarfire = {
     imports = [ <plasma-manager/modules> ];
-    programs = {
-      plasma = {
-        enable = true;
-        overrideConfig = true;
-        input.keyboard.numlockOnStartup = "on";
+    programs.plasma = {
+      enable = true;
+      overrideConfig = true;
+      input.keyboard.numlockOnStartup = "on";
 
-        kscreenlocker = {
-          autoLock = false;
-          appearance.wallpaper = "/home/solarfire/Pictures/Wallpapers/97.jpg";
-        };
+      kscreenlocker = {
+        autoLock = false;
+        appearance.wallpaper = "/home/solarfire/Pictures/Wallpapers/97.jpg";
+      };
 
-        shortcuts = {
-          kwin = {
-            "Window Minimize" = "Alt+F9";
-            "Window Maximize" = "Alt+F10";
-          };
-          "services/kitty.desktop"."_launch" = "Ctrl+Alt+T";
-          "services/org.kde.konsole.desktop"."_launch" = "None";
+      shortcuts = {
+        kwin = {
+          "Window Minimize" = "Alt+F9";
+          "Window Maximize" = "Alt+F10";
         };
+        "services/kitty.desktop"."_launch" = "Ctrl+Alt+T";
+        "services/org.kde.konsole.desktop"."_launch" = "None";
+      };
 
-        powerdevil.AC = {
-          autoSuspend.action = "nothing";
-          turnOffDisplay.idleTimeout = "never";
-          dimDisplay.enable = false;
-        };
+      powerdevil.AC = {
+        autoSuspend.action = "nothing";
+        turnOffDisplay.idleTimeout = "never";
+        dimDisplay.enable = false;
       };
     };
   };
