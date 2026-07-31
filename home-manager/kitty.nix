b69913d19f8fbd2f27d06536fe9909bd434218cd
solarfire <xatra169@gmail.com>
2026-07-31 10:31:33 -0400

    home-manager/kitty: change cursor color to mint green to match zsh prompt

--- home-manager/kitty.nix
+++ home-manager/kitty.nix
@@ -1,29 +1,44 @@
+{ lib, ... }:
+let
+  themeFileAppendContents = lib.mkOrder 1500 ''
+    cursor #5fff87
+  '';
+in
 {
-  home-manager.users.solarfire.programs.kitty = {
-    enable = true;
-    autoThemeFiles.dark = "Argonaut";
-    autoThemeFiles.light = "Argonaut";
-    autoThemeFiles.noPreference = "Argonaut";
+  home-manager.users.solarfire = {
+    xdg.configFile = {
+      "kitty/dark-theme.auto.conf".text = themeFileAppendContents;
+      "kitty/light-theme.auto.conf".text = themeFileAppendContents;
+      "kitty/no-preference-theme.auto.conf".text = themeFileAppendContents;
+    };
 
-    font.name = "Miracode Nerdfont";
-    font.size = 9;
+    programs.kitty = {
+      enable = true;
+      autoThemeFiles.dark = "Argonaut";
+      autoThemeFiles.light = "Argonaut";
+      autoThemeFiles.noPreference = "Argonaut";
 
-    shellIntegration.mode = "no-title no-cursor";
+      font.name = "Miracode Nerdfont";
+      font.size = 10;
 
-    settings = {
-      font_features = "MiracodeNF -liga";
+      shellIntegration.mode = "no-title no-cursor";
 
-      background_opacity = 0.7;
+      settings = {
+        font_features = "MiracodeNF -liga";
 
-      scrollback_lines = 5000;
-      wheel_scroll_multiplier = 1;
+        background_opacity = 0.7;
+        background_blur = 1;
 
-      cursor_shape = "block";
-      cursor_blink_interval = 0.5;
-      cursor_trail = 1;
+        scrollback_lines = 5000;
+        wheel_scroll_multiplier = 1;
 
-      mouse_hide_wait = 3.0;
-      detect_urls = "yes";
+        cursor_shape = "block";
+        cursor_blink_interval = 0.5;
+        cursor_trail = 1;
+
+        mouse_hide_wait = 3.0;
+        detect_urls = "yes";
+      };
     };
   };
 }
