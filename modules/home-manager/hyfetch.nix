4e8393865a026ab28f853802222bc5a74e0eaefe
solarfire <xatra169@gmail.com>
2026-08-14 12:46:12 -0400

    home-manager/hyfetch: made pride flag stripes horizontal

--- modules/home-manager/hyfetch.nix
+++ modules/home-manager/hyfetch.nix
@@ -7,7 +7,7 @@
       mode = "rgb";
       light_dark = "dark";
       lightness = 0.5;
-      color_align.mode = "vertical";
+      color_align.mode = "horizontal";
       backend = "fastfetch";
       pride_month_disable = false;
     };
