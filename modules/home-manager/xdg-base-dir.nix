fb10088d89f310aff4a232a206d582234fe93bfe
solarfire <xatra169@gmail.com>
2026-08-14 12:35:39 -0400

    home-manager/xdg-base-dirs: replace XDG_*_HOME variables with hardcoded strings to the same paths

--- modules/home-manager/xdg-base-dir.nix
+++ modules/home-manager/xdg-base-dir.nix
@@ -2,22 +2,17 @@
 # Base Directory specification
 {
   home.sessionVariables = {
-    XDG_CACHE_HOME = "/home/solarfire/.cache";
-    XDG_CONFIG_HOME = "/home/solarfire/.config";
-    XDG_STATE_HOME = "/home/solarfire/.local/state";
-    XDG_DATA_HOME = "/home/solarfire/.local/share";
+    CUDA_CACHE_PATH = "$HOME/.cache/nv";
 
-    CUDA_CACHE_PATH = "$XDG_CACHE_HOME/nv";
+    GTK2_RC_FILES = "$HOME/.config/gtk-2.0/gtkrc";
+    WGETRC = "$HOME/.config/wgetrc";
 
-    GTK2_RC_FILES = "$XDG_CONFIG_HOME/gtk-2.0/gtkrc";
-    WGETRC = "$XDG_CONFIG_HOME/wgetrc";
+    HISTFILE = "$HOME/.local/state/bash_history";
 
-    HISTFILE = "$XDG_STATE_HOME/bash_history";
-
-    ANDROID_USER_HOME = "$XDG_DATA_HOME/android";
-    GNUPGHOME = "$XDG_DATA_HOME/gnupg";
-    RUSTUP_HOME = "$XDG_DATA_HOME/rustup";
-    CARGO_HOME = "$XDG_DATA_HOME/cargo";
-    WINEPREFIX = "$XDG_DATA_HOME/wine";
+    ANDROID_USER_HOME = "$HOME/.local/share/android";
+    GNUPGHOME = "$HOME/.local/share/gnupg";
+    RUSTUP_HOME = "$HOME/.local/share/rustup";
+    CARGO_HOME = "$HOME/.local/share/cargo";
+    WINEPREFIX = "$HOME/.local/share/wine";
   };
 }
