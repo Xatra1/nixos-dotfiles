e38789a40cad3aa5365ff7332ea526c7d5bf02c7
solarfire <xatra169@gmail.com>
2026-08-17 10:16:17 -0400

    plasma-manager/input: add mouse settings

--- modules/home-manager/plasma/input.nix
+++ modules/home-manager/plasma/input.nix
@@ -2,6 +2,15 @@
   programs.plasma.input = {
     keyboard.options = [ "caps:none" ];
 
+     mice = [
+      {
+        name = "PixArt Dell MS116 USB Optical Mouse";
+        vendorId = "413c";
+        productId = "301a";
+        acceleration = 0.4;
+      }
+    ];
+
     touchpads = [
       {
         name = "ETPS/2 Elantech Touchpad";
