7aa4a9aa1d91f712bc74fb4ff0902a6c850bf65d
solarfire <xatra169@gmail.com>
2026-08-19 12:15:46 -0400

    plasma-manager/input+extra-config-files: move kcminputrc setting to input.nix

--- modules/home-manager/plasma/extra-config-files.nix
+++ modules/home-manager/plasma/extra-config-files.nix
@@ -7,8 +7,6 @@
       PreviewSize = 32;
     };
 
-    kcminputrc."Libinput/14648/4616/2.4G Wireless Keyboard Mouse".ScrollMethod = 4;
-
     kdeglobals = {
       KDE.DndBehavior = "MoveIfSameDevice";
 
--- modules/home-manager/plasma/input.nix
+++ modules/home-manager/plasma/input.nix
@@ -1,24 +1,28 @@
 {
-  programs.plasma.input = {
-    keyboard.options = [ "caps:none" ];
+  programs.plasma = {
+    configFile.kcminputrc."Libinput/14648/4616/2.4G Wireless Keyboard Mouse".ScrollMethod = 4;
 
-     mice = [
-      {
-        name = "2.4G Wireless Keyboard Mouse"; # how creative
-        vendorId = "3938";
-        productId = "1208";
-        acceleration = 0.3;
-        scrollSpeed = 1.5;
-      }
-    ];
+    input = {
+      keyboard.options = [ "caps:none" ];
 
-    touchpads = [
-      {
-        name = "ETPS/2 Elantech Touchpad";
-        vendorId = "0002";
-        productId = "000e";
-        pointerSpeed = 0.3;
-      }
-    ];
+      mice = [
+        {
+          name = "2.4G Wireless Keyboard Mouse"; # how creative
+          vendorId = "3938";
+          productId = "1208";
+          acceleration = 0.3;
+          scrollSpeed = 1.5;
+        }
+      ];
+
+      touchpads = [
+        {
+          name = "ETPS/2 Elantech Touchpad";
+          vendorId = "0002";
+          productId = "000e";
+          pointerSpeed = 0.3;
+        }
+      ];
+    };
   };
 }
