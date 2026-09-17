3963cc4fec9b7344139ac0fb0d8e6a100c64ee66
solarfire <xatra169@gmail.com>
2026-09-17 15:41:32 -0400

    chore+packages: update ioquake3 package hash

--- modules/packages.nix
+++ modules/packages.nix
@@ -13,7 +13,7 @@ let
 
   ioquake3 = pkgs.callPackage (pkgs.fetchurl {
     url = "https://codeberg.org/solarfire/nix-derivations/raw/branch/master/ioquake3/package.nix";
-    hash = "sha256-nDr5NZH8JLKJonYbcqOG4wO/m3PE4qyeFw2o36H48DQ=";
+    hash = "sha256-CAs+HIteMMYlZmCsoowzTdm3fv2PA/GfKgPFlxcHdcI=";
   }) { };
 
   ioquake3_overlay = (
